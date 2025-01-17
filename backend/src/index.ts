f5bb8d473e4d5adc1038da92db557b6a9b4ce921
Siddhi Sahu <himanisahu739@gmail.com>
2025-01-17 12:34:28 +0530

    add base code

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+
+const app = express();
+app.use(cors());
+
+const httpServer = createServer(app);
+
+
+const port = process.env.PORT || 3001;
+httpServer.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
