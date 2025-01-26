93d3b3686aaf7d35694935d69691822f9efbdb5f
Siddhi Sahu <himanisahu739@gmail.com>
2025-01-26 16:11:13 +0530

    add ws.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,14 @@ import express from "express";
 import cors from "cors";
 import { createServer } from "http";
 import { initHTTP } from "./http";
+import { initWs } from "./ws";
 
 const app = express();
 app.use(cors());
 
 const httpServer = createServer(app);
 
+initWs(httpServer);
 initHTTP(app);
 
 
---
+++ backend/src/ws.ts
@@ -0,0 +1,13 @@
+import { Server as HttpServer } from "http";
+import { Server } from "socket.io";
+
+export async function initWs(httpServer: HttpServer) {
+    //initial a web socket serverr for 2 way connection
+    const io = new Server(httpServer, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    })
+
+}
